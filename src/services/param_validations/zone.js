8f8c22fcaaa6efdf45e850b58ff2ea4c5b3e9112
Vincenzo Bianco <vincenzo_bianco_93@hotmail.com>
2017-07-04 17:08:09 -0400

    add param_validations to create & read zones

---
+++ src/services/param_validations/zone.js
@@ -0,0 +1,26 @@
+import objectId from 'joi-objectid';
+import Joi from 'joi';
+
+Joi.objectId = objectId(Joi);
+
+export default {
+  readAll: {
+    query: {
+      offset: Joi.number().integer(),
+      limit: Joi.number().integer(),
+    },
+  },
+
+  create: {
+    body: {
+      name: Joi.string().required(),
+      description: Joi.string(),
+      address: Joi.string(),
+      type: Joi.string(),
+      geometry: Joi.object()
+        .keys({
+          coordinates: Joi.array().items(Joi.number()).required(),
+        }),
+    },
+  },
+};
