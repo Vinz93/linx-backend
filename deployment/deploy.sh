8e5ccbe3b374b6bed9e8fbe2f254394a41d5fc09
Isaac Arismendi <arismendi6702@gmail.com>
2017-07-31 14:20:55 -0400

    Deployment script enhancement

--- deployment/deploy.sh
+++ deployment/deploy.sh
@@ -1,1 +1,1 @@
-ssh $1@$2 "cd /var/www/backend && git checkout origin/develop && git pull origin develop && cd /var/www/backend && npm install && npm run build && pm2 restart all"
+ssh $1@$2 "cd /var/www/backend && git checkout origin/develop && git pull origin develop && cd /var/www/backend && rm package-lock.json && npm install && npm run build && pm2 restart all"
