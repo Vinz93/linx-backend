45af1953aff191a57082f28537c6a5379f17030c
terra <rafaa.lb@hotmail.com>
2017-08-02 16:30:11 -0400

    return terminals

--- src/controllers/user.js
+++ src/controllers/user.js
@@ -317,10 +317,10 @@ const UserController = {
 
   async atAirport(req, res) {
     const { longitude, latitude } = req.query;
-    const zone = await Zone.findOne({ geometry: { $geoIntersects: { $geometry: { type: "Point", coordinates: [longitude, latitude] } } } });
+    const zone = await Zone.findOne({ geometry: { $geoIntersects: { $geometry: { type: "Point", coordinates: [longitude, latitude] } } }, type: "airport" });
     if (zone) {
-      const { name } = zone;
-      return res.json({ name, isAt: true });
+      const { name, terminals } = zone;
+      return res.json({ name, terminals, isAt: true });
     }
     return res.json({ isAt: false });
   },
