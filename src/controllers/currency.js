3a709517eac5f372854cd6b76cc53fbab32090c9
Vincenzo Bianco <vincenzo_bianco_93@hotmail.com>
2017-08-10 16:19:18 -0400

    change dev config

--- src/controllers/currency.js
+++ src/controllers/currency.js
@@ -1,8 +1,10 @@
 import httpStatus from 'http-status';
 import axios from 'axios';
-import { APIError } from '../helpers/errors';
 
-const url = `http://apilayer.net/api/list`;
+import config from '../config/env';
+import { APIError } from '../helpers/errors';
+const { apiUrl, accessKey } = config.currency.currencyLayer;
+
 const CurrencyController = {
   /**
    * @swagger
@@ -15,17 +17,17 @@ const CurrencyController = {
    *       - application/json
    *     responses:
    *       200:
-   *         description: return an array of currencies'
+   *         description: return an object of currencies'
    */
 
   async list(req, res) {
-    const response = await axios.get(url, {
+    const response = await axios.get(`${apiUrl}/list`, {
       params: {
-        access_key: `db711ea12fbe3e5f97f2a05ed470ad45`,
+        access_key: accessKey,
       },
     });
     if (!response.data.success) {
-      throw new APIError(httpStatus.BAD_REQUEST, 'error getting currencies');
+      throw new APIError(httpStatus.BAD_REQUEST, 'Error getting currencies');
     }
     res.status(httpStatus.OK).json(response.data.currencies);
   },
