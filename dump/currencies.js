d64bef4b6d5e796ee3aec73485b6236e6c7af1f2
Vincenzo Bianco <vincenzo_bianco_93@hotmail.com>
2017-08-16 13:44:50 -0400

    change seeds

---
+++ dump/currencies.js
@@ -0,0 +1,228 @@
+const currencies = [
+  {
+    currencyKey: "USD",
+    name: "dolares americanos",
+    denominations: [
+      {
+        coinType: "bill",
+        value: 100,
+      },
+      {
+        coinType: "coin",
+        value: 0.5,
+      },
+      {
+        coinType: "bill",
+        value: 1,
+      },
+      {
+        coinType: "bill",
+        value: 5,
+      },
+      {
+        coinType: "bill",
+        value: 10,
+      },
+      {
+        coinType: "bill",
+        value: 20,
+      },
+      {
+        coinType: "bill",
+        value: 50,
+      },
+      {
+        coinType: "coin",
+        value: 0.25,
+      },
+    ],
+  },
+  {
+    currencyKey: "CAD",
+    name: "Canadian Dollar",
+    denominations: [
+      {
+        coinType: "bill",
+        value: 1,
+      },
+      {
+        coinType: "coin",
+        value: 0.25,
+      },
+      {
+        coinType: "coin",
+        value: 0.5,
+      },
+      {
+        coinType: "bill",
+        value: 5,
+      },
+      {
+        coinType: "bill",
+        value: 10,
+      },
+      {
+        coinType: "bill",
+        value: 20,
+      },
+      {
+        coinType: "bill",
+        value: 50,
+      },
+      {
+        coinType: "bill",
+        value: 100,
+      },
+    ],
+  },
+  {
+    currencyKey: "EUR",
+    name: "Euro",
+    denominations: [
+      {
+        coinType: "bill",
+        value: 1,
+      },
+      {
+        coinType: "coin",
+        value: 0.25,
+      },
+      {
+        coinType: "coin",
+        value: 0.5,
+      },
+      {
+        coinType: "bill",
+        value: 5,
+      },
+      {
+        coinType: "bill",
+        value: 10,
+      },
+      {
+        coinType: "bill",
+        value: 20,
+      },
+      {
+        coinType: "bill",
+        value: 50,
+      },
+      {
+        coinType: "bill",
+        value: 100,
+      },
+    ],
+  },
+  {
+    currencyKey: "GBP",
+    name: "British Pound Sterling",
+    denominations: [
+      {
+        coinType: "bill",
+        value: 1,
+      },
+      {
+        coinType: "bill",
+        value: 100,
+      },
+      {
+        coinType: "bill",
+        value: 50,
+      },
+      {
+        coinType: "bill",
+        value: 20,
+      },
+      {
+        coinType: "bill",
+        value: 10,
+      },
+      {
+        coinType: "bill",
+        value: 5,
+      },
+      {
+        coinType: "coin",
+        value: 0.5,
+      },
+      {
+        coinType: "coin",
+        value: 0.25,
+      },
+    ],
+  },
+  {
+    currencyKey: "VEF",
+    name: "Venezuelan Bolivar Fuerte",
+    denominations: [
+      {
+        coinType: "bill",
+        value: 100,
+      },
+      {
+        coinType: "bill",
+        value: 500,
+      },
+      {
+        coinType: "bill",
+        value: 1000,
+      },
+      {
+        coinType: "bill",
+        value: 2000,
+      },
+      {
+        coinType: "bill",
+        value: 5000,
+      },
+      {
+        coinType: "bill",
+        value: 10000,
+      },
+      {
+        coinType: "bill",
+        value: 20000,
+      },
+      {
+        coinType: "coin",
+        value: 50,
+      },
+    ],
+  },
+  {
+    currencyKey: "SEK",
+    name: "Swedish Kron",
+    denominations: [
+      {
+        coinType: "bill",
+        value: 1,
+      },
+      {
+        coinType: "coin",
+        value: 0.5,
+      },
+      {
+        coinType: "bill",
+        value: 5,
+      },
+      {
+        coinType: "bill",
+        value: 10,
+      },
+      {
+        coinType: "bill",
+        value: 20,
+      },
+      {
+        coinType: "bill",
+        value: 50,
+      },
+      {
+        coinType: "bill",
+        value: 100,
+      },
+    ],
+  },
+];
+
+export default currencies;
---
+++ dump/index.js
@@ -0,0 +1,37 @@
+import mongoose from 'mongoose';
+import Promise from 'bluebird';
+
+import config from '../src/config/env';
+import Currency from '../src/models/currency';
+import currenciesData from './currencies';
+
+mongoose.Promise = Promise;
+
+async function loadData() {
+  try {
+    console.log(`Running seeds 👷`);
+    await Currency.remove({});
+    await Currency.create(currenciesData);
+    mongoose.connection.close();
+    console.log(`The process finished :white_check_mark:`);
+    process.exit();
+  } catch (err) {
+    console.log(`❌`, err);
+  }
+}
+
+function connect() {
+  const options = {
+    server: {
+      socketOptions: {
+        keepAlive: 1,
+      },
+    },
+  };
+  return mongoose.connect(config.dbConfig.db, options).connection;
+}
+
+connect()
+.on('error', console.log)
+.on('disconnected', connect)
+.once('open', loadData);
