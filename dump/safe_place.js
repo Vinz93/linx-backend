efd77cccb89474f5383633977eb8f2eb75b4ab23
Isaac Arismendi <arismendi6702@gmail.com>
2017-09-21 19:10:09 -0400

    data dump

--- dump/safe_place.js
+++ dump/safe_place.js
@@ -4,10 +4,65 @@ const safePlace = [
     description: 'description',
     address: 'description of the address',
     category: 'coffee',
+    phone: '+585555555551',
+    relevance: 2,
+    location: {
+      coordinates: [-66.855285, 10.491647],
+    },
+  },
+  {
+    name: 'local 02',
+    description: 'description',
+    address: 'description of the address',
+    category: 'coffee',
+    phone: '+585555555552',
+    relevance: 2,
+    location: {
+      coordinates: [-66.851315, 10.492596],
+    },
+  },
+  {
+    name: 'local 03',
+    description: 'description',
+    address: 'description of the address',
+    category: 'coffee',
+    phone: '+585555555553',
+    relevance: 2,
+    location: {
+      coordinates: [-66.856765, 10.488566],
+    },
+  },
+  {
+    name: 'local 04',
+    description: 'description',
+    address: 'description of the address',
+    category: 'coffee',
+    phone: '+585555555554',
+    relevance: 2,
+    location: {
+      coordinates: [-66.852066, 10.497354],
+    },
+  },
+  {
+    name: 'local 05',
+    description: 'description',
+    address: 'description of the address',
+    category: 'coffee',
     phone: '+585555555555',
     relevance: 2,
     location: {
-      coordinates: [-66.996423, 10.595716],
+      coordinates: [-66.850028, 10.499126],
+    },
+  },
+  {
+    name: 'local 06',
+    description: 'description',
+    address: 'description of the address',
+    category: 'coffee',
+    phone: '+585555555556',
+    relevance: 2,
+    location: {
+      coordinates: [-66.866722, 10.492955],
     },
   },
 ];
