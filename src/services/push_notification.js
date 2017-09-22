0dca024460a04c8737d28b2fe040d79b31b036dd
Isaac Arismendi <arismendi6702@gmail.com>
2017-09-22 11:09:52 -0400

    validation for push notifiacation

--- src/services/push_notification.js
+++ src/services/push_notification.js
@@ -4,11 +4,11 @@ import GCM from 'node-gcm';
 const { gcmconfig } = config.pushnotifications;
 const { apnconfig } = config.pushnotifications;
 const sender = new GCM.Sender(gcmconfig);
-const apnProvider = new apn.Provider(apnconfig);
 const note = new apn.Notification();
 
 export async function contact(pushData, requestedToken, deviceRequested, message, pushType) {
   // APN CONFIG
+  const apnProvider = new apn.Provider(apnconfig);
   note.expiry = Math.floor(Date.now() / 1000) + 3600; // Expires 1 hour from now.
   note.sound = 'ping.aiff';
   note.alert = message;
