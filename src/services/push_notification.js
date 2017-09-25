f31dc1686356b6f8a6879b59ad0132d2edfed230
Isaac Arismendi <iarismendi@solsteace.com>
2017-09-25 10:30:49 -0400

    test  logic push notification

--- src/services/push_notification.js
+++ src/services/push_notification.js
@@ -2,25 +2,23 @@ import config from '../config/env';
 import apn from 'apn';
 import GCM from 'node-gcm';
 const { gcmconfig, apnconfig } = config.pushnotifications;
+const apnProvider = new apn.Provider(apnconfig);
+const sender = new GCM.Sender(gcmconfig);
 
 export async function contact(pushData, requestedToken, deviceRequested, message, pushType) {
   if (deviceRequested === "ios") {
     // APN CONFIG
-    const apnProvider = new apn.Provider(apnconfig);
     const note = new apn.Notification();
     note.expiry = Math.floor(Date.now() / 1000) + 3600; // Expires 1 hour from now.
     note.sound = 'ping.aiff';
     note.alert = message;
     note.payload = { ...pushData, pushType };
     note.topic = "com.solsteace.linxios";
-
     // END APN CONFIG
     console.info('requestedToken: ', requestedToken, JSON.stringify(note));
     const apnpush = await apnProvider.send(note, requestedToken);
     return apnpush;
   }
-
-  const sender = new GCM.Sender(gcmconfig);
   const gcmmessage = new GCM.Message({
     data: {
       message,
