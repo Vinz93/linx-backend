24e44e5e8efb46ad5f7e2fc1d6e0476bac44ab24
Isaac Arismendi <arismendi6702@gmail.com>
2017-09-26 09:55:23 -0400

    validation push notification

--- src/services/push_notification.js
+++ src/services/push_notification.js
@@ -1,11 +1,21 @@
-import config from '../config/env';
 import apn from 'apn';
 import GCM from 'node-gcm';
+import httpStatus from 'http-status';
+import { APIError } from '../helpers/errors';
+import config from '../config/env';
+
 const { gcmconfig, apnconfig } = config.pushnotifications;
 const apnProvider = new apn.Provider(apnconfig);
 const sender = new GCM.Sender(gcmconfig);
 
 export async function contact(pushData, requestedToken, deviceRequested, message, pushType) {
+  if (!requestedToken) {
+    throw new APIError('deviceToken match not found', httpStatus.NOT_FOUND);
+  }
+  if (!deviceRequested) {
+    throw new APIError('deviceType match not found', httpStatus.NOT_FOUND);
+  }
+
   if (deviceRequested === "ios") {
     // APN CONFIG
     const note = new apn.Notification();
